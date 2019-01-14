1ce3098258df4dcc34e7f8bdbeacc13b082e7f1d
Pavel Afanasev <afansvme@yandex.ru>
2019-01-14 22:55:26 +0300

    add medium obfuscation options

--- app.js
+++ app.js
@@ -13,7 +13,27 @@ app.use(bodyParser.json());
 
 app.post("/", function(req, res) {
     let obfuscationResult = JavaScriptObfuscator.obfuscate(
-        Buffer.from(req.body.src, "base64").toString("utf-8")
+        Buffer.from(req.body.src, "base64").toString("utf-8"),
+        {
+            compact: true,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 0.75,
+            deadCodeInjection: true,
+            deadCodeInjectionThreshold: 0.4,
+            debugProtection: false,
+            debugProtectionInterval: false,
+            disableConsoleOutput: false,
+            identifierNamesGenerator: "hexadecimal",
+            log: false,
+            renameGlobals: false,
+            rotateStringArray: true,
+            selfDefending: true,
+            stringArray: true,
+            stringArrayEncoding: "base64",
+            stringArrayThreshold: 0.75,
+            transformObjectKeys: true,
+            unicodeEscapeSequence: false
+        }
     );
 
     res.send(
